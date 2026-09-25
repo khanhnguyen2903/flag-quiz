be43a713fc4b51e6cb397f2a7edadcde8866cbff
Khanh Nguyen <nvkhanh084@gmail.com>
2026-09-25 09:18:52 +0700

    update quiz_flag.json

--- quiz/views.py
+++ quiz/views.py
@@ -1,19 +1,29 @@
+import json
+from pathlib import Path
+
+from django.conf import settings
 from django.shortcuts import render
-from .models import Flag
+
 
 def game(request):
-    flags = Flag.objects.all()
-    flag_data = [ 
-        { 
-            "id": flag.id, 
-            "country_name": flag.country_name, 
-            "capital_name" : flag.capital_name,
-            "continent" : flag.continent,
-            "url_flag": flag.url_flag, 
-        } 
-        for flag in flags 
-    ]
-    context = {
-        "flags": flag_data
-    }
-    return render(request, 'quiz/game.html', context)
+    json_file = Path(settings.BASE_DIR) / "quiz_flag.json"
+
+    with open(json_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    flags = []
+
+    for item in data:
+        flag = {
+            "id": item["pk"],
+            "country_name": item["fields"]["country_name"],
+            "capital_name": item["fields"]["capital_name"],
+            "continent": item["fields"]["continent"],
+            "url_flag": item["fields"]["url_flag"],
+        }
+
+        flags.append(flag)
+
+    return render(request, "quiz/game.html", {
+        "flags": flags
+    })
